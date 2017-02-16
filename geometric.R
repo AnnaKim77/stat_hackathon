674ccf1dedac16ea1022bf31642ef518b3cd5eed
SangwanKIM <novavitas@gmail.com>
2017-02-17 01:43:13 +0900

    code

--- geometric.R
+++ geometric.R
@@ -12,7 +12,6 @@ library(maps)
 library(car)
 library(corrplot)
 library(ggplot2)
-library(lmtest)
 
 suppressPackageStartupMessages({
   library(dplyr)
@@ -21,10 +20,12 @@ suppressPackageStartupMessages({
   library(magrittr)
 })
 
+#data eraser
 rm(list=ls(all=TRUE))
 
+#working directory
 setwd("C:/Users/user/Desktop/hackathon/dataset")
-metal <- read.csv("metal.csv",header = T)
+metal <- read.csv("metal_or.csv",header = T)
 grid <- read.csv("seoul_grid.csv",header = T)
 metal_kri <- read.csv("metal_kri.csv",header = T)
 
@@ -33,6 +34,7 @@ metal_kri$Month <- as.factor(metal_kri$Month)
 metal$Year <- as.factor(metal$Year)
 metal_kri$Year <- as.factor(metal_kri$Year)
 
+#좌표값 입력
 metal <- merge(metal, grid, by="Loca", all.x = T)
 metal_kri <- merge(metal_kri, grid, by="Loca", all.x = T)
 
@@ -40,14 +42,6 @@ summary(metal)
 
 corrplot(cor(metal[,c(-1,-8,-9,-10)]),method = "number")
 corrplot(cor(metal[,c(-1,-8,-9,-10)]),method = "circle")
-
-for(i in c(3,4,5,6,7,8)){
-  metal[,i] <- scale(metal[,i])
-}
-
-for(i in c(3,4,5,6,7)){
-  metal_kri[,i] <- scale(metal_kri[,i])
-}
 
 met_lm <- lm(log(Pb)~.,data = metal[,c(-1,-8,-9,-10)])
 
@@ -59,7 +53,8 @@ par(mfrow = c(1,1))
 
 step <- step(met_lm, direction = "backward")
 
-met_lm_red <- lm(log(Pb) ~ NO2+O3+CO+PM25, data = metal)
+met_lm_red <- lm(log(Pb) ~ NO2 + O3 + CO + PM25, data = metal)
+
 summary(met_lm_red)
 vif(met_lm_red)
 
@@ -71,51 +66,108 @@ ncvTest(met_lm_red)
 
 shapiro.test(residuals(met_lm_red))
 
-metal$resid = residuals(met_lm_red)
-
-#month/year 데이터set 구성
-
-metal_mon1 <- metal[metal$Year == 2015,]
-metal_kri_mon1 <- metal_kri[metal_kri$Year == 2015,]
-
-summary(metal_kri)
-
+#예측지역 시각화
 myMap <- get_map(location="seoul", source="google", maptype="roadmap", crop=FALSE, zoom = 11) 
 
 plot1 = ggmap(myMap)+ geom_point(aes(x = lon, y = lat), 
-                                data = metal_mon1, alpha = .5, color="darkred", size = 1) + coord_equal() + 
+                                data = metal, alpha = .5, color="darkred", size = 1) + coord_equal() + 
   ggtitle("Points with measurements") + theme(plot.title = element_text(hjust = 0.5))
 
 plot2=ggmap(myMap)+ geom_point(aes(x = lon, y = lat), 
-                               data = metal_kri_mon1, alpha = .5, color="darkred", size = 1) + coord_equal() + 
+                               data = metal_kri, alpha = .5, color="darkred", size = 1) + coord_equal() + 
   ggtitle("Points at which to estimate") + theme(plot.title = element_text(hjust = 0.5))
 
 grid.arrange(plot1, plot2, ncol = 2)
 
-#coordinates(metal_mon1) = ~ lat + lon
-#bbox(metal_mon1)
+#회귀직선이용 예측값사용
+metal_kri$pred <- exp(predict(met_lm_red,metal_kri))
 
-#sample variogram values
-#dvgm <- variogram(resid ~ 1, data = metal_mon1)  
-#dfit <- fit.variogram(dvgm, vgm("Gau"))
-#plot(dvgm)
-#plot(dvgm, dfit)
-#
-#kriged <- krige(log(resid) ~ 1, metal_2010, metal_kri_2010, model=dfit)
-#dk=as.data.frame(kriged)
 
-metal_kri_mon1$pred <- exp(predict(met_lm_red,metal_kri_mon1))
-
-x1.mm=metal_kri_mon1$lon
-x2.mm=metal_kri_mon1$lat
-
-x1r = range(metal_kri$lon)
-x2r = range(metal_kri$lat)
-y1 = metal_kri_mon1$pred
+#예측시각화 (2015년)
+x1.15 <- metal_kri[metal_kri$Year == 2015,10]
+x2.15 <- metal_kri[metal_kri$Year == 2015,11]
+x1r <- range(metal_kri$lon)
+x2r <- range(metal_kri$lat)
+y2015 <- metal_kri[metal_kri$Year == 2015,12]
 
 mylocation=c(x1r[1],x2r[1],x1r[2],x2r[2])
 
-int.data=interp(x1.mm,x2.mm,y1,duplicate="strip")
+int.data=interp(x1.15, x2.15, y2015, duplicate="strip")
+map("world",xlim=x1r,ylim=x2r,lwd=2)
+image(int.data,col=heat.colors(100),add=T)
+contour(int.data,add=T,col=4,lwd=1,labcex=1)
+map("world",xlim=x1r,ylim=x2r,lwd=2,add=T)
+
+#예측시각화 (2014년)
+x1.14 <- metal_kri[metal_kri$Year == 2014,10]
+x2.14 <- metal_kri[metal_kri$Year == 2014,11]
+x1r <- range(metal_kri$lon)
+x2r <- range(metal_kri$lat)
+y2014 <- metal_kri[metal_kri$Year == 2014,12]
+
+mylocation=c(x1r[1],x2r[1],x1r[2],x2r[2])
+
+int.data=interp(x1.14, x2.14, y2014, duplicate="strip")
+map("world",xlim=x1r,ylim=x2r,lwd=2)
+image(int.data,col=heat.colors(100),add=T)
+contour(int.data,add=T,col=4,lwd=1,labcex=1)
+map("world",xlim=x1r,ylim=x2r,lwd=2,add=T)
+
+#예측시각화 (2013년)
+x1.13 <- metal_kri[metal_kri$Year == 2013,10]
+x2.13 <- metal_kri[metal_kri$Year == 2013,11]
+x1r <- range(metal_kri$lon)
+x2r <- range(metal_kri$lat)
+y2013 <- metal_kri[metal_kri$Year == 2013,12]
+
+mylocation=c(x1r[1],x2r[1],x1r[2],x2r[2])
+
+int.data=interp(x1.13, x2.13, y2013, duplicate="strip")
+map("world",xlim=x1r,ylim=x2r,lwd=2)
+image(int.data,col=heat.colors(100),add=T)
+contour(int.data,add=T,col=4,lwd=1,labcex=1)
+map("world",xlim=x1r,ylim=x2r,lwd=2,add=T)
+
+#예측시각화 (2012년)
+x1.12 <- metal_kri[metal_kri$Year == 2012,10]
+x2.12 <- metal_kri[metal_kri$Year == 2012,11]
+x1r <- range(metal_kri$lon)
+x2r <- range(metal_kri$lat)
+y2012 <- metal_kri[metal_kri$Year == 2012,12]
+
+mylocation=c(x1r[1],x2r[1],x1r[2],x2r[2])
+
+int.data=interp(x1.12, x2.12, y2012, duplicate="strip")
+map("world",xlim=x1r,ylim=x2r,lwd=2)
+image(int.data,col=heat.colors(100),add=T)
+contour(int.data,add=T,col=4,lwd=1,labcex=1)
+map("world",xlim=x1r,ylim=x2r,lwd=2,add=T)
+
+#예측시각화 (2011년)
+x1.11 <- metal_kri[metal_kri$Year == 2011,10]
+x2.11 <- metal_kri[metal_kri$Year == 2011,11]
+x1r <- range(metal_kri$lon)
+x2r <- range(metal_kri$lat)
+y2011 <- metal_kri[metal_kri$Year == 2011,12]
+
+mylocation=c(x1r[1],x2r[1],x1r[2],x2r[2])
+
+int.data=interp(x1.11, x2.11, y2011, duplicate="strip")
+map("world",xlim=x1r,ylim=x2r,lwd=2)
+image(int.data,col=heat.colors(100),add=T)
+contour(int.data,add=T,col=4,lwd=1,labcex=1)
+map("world",xlim=x1r,ylim=x2r,lwd=2,add=T)
+
+#예측시각화 (2010년)
+x1.10 <- metal_kri[metal_kri$Year == 2010,10]
+x2.10 <- metal_kri[metal_kri$Year == 2010,11]
+x1r <- range(metal_kri$lon)
+x2r <- range(metal_kri$lat)
+y2010 <- metal_kri[metal_kri$Year == 2010,12]
+
+mylocation=c(x1r[1],x2r[1],x1r[2],x2r[2])
+
+int.data=interp(x1.10, x2.10, y2010, duplicate="strip")
 map("world",xlim=x1r,ylim=x2r,lwd=2)
 image(int.data,col=heat.colors(100),add=T)
 contour(int.data,add=T,col=4,lwd=1,labcex=1)
